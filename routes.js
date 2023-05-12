b8ae96e5260acaab06812aa1f3d36b11981d67d0
rizky fahriza noor <rizkiboo08@gmail.com>
2023-05-12 09:54:45 +0700

    initial commit

---
+++ routes.js
@@ -0,0 +1,16 @@
+const routes = [
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+];
