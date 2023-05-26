c44c306f0f908193a2ad6829bf259a8dffd8456c
rizky fahriza noor <rizkiboo08@gmail.com>
2023-05-26 16:17:21 +0700

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,20 @@
 module.exports = {
     "env": {
         "browser": true,
-        "commonjs": true,
         "es2021": true
     },
     "extends": [
         "eslint:recommended",
-        "plugin:vue/vue3-essential",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:react/recommended"
     ],
     "overrides": [
     ],
-    "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaVersion": "latest"
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
     "plugins": [
-        "vue",
-        "@typescript-eslint"
+        "react"
     ],
     "rules": {
     }
